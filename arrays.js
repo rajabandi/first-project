153ee4a0559bbf5d3b2160fd3738895682b14904
Your Name <you@example.com>
2021-07-27 16:37:41 +0530

    index

--- arrays.js
+++ arrays.js
@@ -1,6 +1,13 @@
-let numbers =[];
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-  
-  console.log(getRandomInt(20));
+let numbers = [];
+for (var number = 0; number < 20; number++) {
+    numbers.push(Math.round(Math.random() * 100));
+}
+console.log(numbers);
+let filteredNumbers =
+    numbers.filter(item => item % 5 === 0)
+console.log(filteredNumbers);
+let sum = numbers.reduce((accumulator, currentValue) => ( accumulator + currentValue));
+console.log(sum);
+console.log(numbers.findIndex((element)=>element % 3 ===0));
+numbers.sort();
+console.log(numbers);
